b629b97dd76be1dfa63130c0ec9ba1a9ed2d139e
Thais dos Santos Oliveira <102552366+olivetha@users.noreply.github.com>
2023-10-20 20:28:35 -0300

    adding branches and project

---
+++ scripts/script.js
@@ -0,0 +1,29 @@
+let editButton = document.querySelector(".profile__edit-button");
+let popup = document.querySelector(".popup");
+
+editButton.addEventListener("click", function () {
+  popup.classList.add("popup__opened");
+  inputName.value = profileName.textContent;
+  inputAbout.value = profileAbout.textContent;
+});
+
+let buttonClose = document.querySelector(".popup__close-button");
+
+buttonClose.addEventListener("click", function () {
+  popup.classList.remove("popup__opened");
+});
+
+let profileName = document.querySelector(".profile__name");
+let profileAbout = document.querySelector(".profile__about");
+let form = document.querySelector(".popup__form");
+let inputName = document.querySelector(".form__input_name");
+let inputAbout = document.querySelector(".form__input_about");
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+  inputName.value = profileName.textContent;
+  inputAbout.value = profileAbout.textContent;
+  popup.classList.remove("popup__opened");
+}
+
+form.addEventListener("submit", handleProfileFormSubmit);
