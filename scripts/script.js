6e861d9e78e1fa1c9bc709bb601f1a3a2745867c
Thais dos Santos Oliveira <102552366+olivetha@users.noreply.github.com>
2023-10-23 10:46:58 -0300

    fix save script and tag list

--- scripts/script.js
+++ scripts/script.js
@@ -1,28 +1,28 @@
-let editButton = document.querySelector(".profile__edit-button");
-let popup = document.querySelector(".popup");
+const editButton = document.querySelector(".profile__edit-button");
+const popup = document.querySelector(".popup");
 
 editButton.addEventListener("click", function () {
-  popup.classList.add("popup_opened");
   inputName.value = profileName.textContent;
   inputAbout.value = profileAbout.textContent;
+  popup.classList.add("popup_opened");
 });
 
-let buttonClose = document.querySelector(".popup__close-button");
+const buttonClose = document.querySelector(".popup__close-button");
 
 buttonClose.addEventListener("click", function () {
   popup.classList.remove("popup_opened");
 });
 
-let profileName = document.querySelector(".profile__name");
-let profileAbout = document.querySelector(".profile__about");
-let form = document.querySelector(".popup__form");
-let inputName = document.querySelector(".form__input_name");
-let inputAbout = document.querySelector(".form__input_about");
+const profileName = document.querySelector(".profile__name");
+const profileAbout = document.querySelector(".profile__about");
+const form = document.querySelector(".popup__form");
+const inputName = document.querySelector(".form__input_name");
+const inputAbout = document.querySelector(".form__input_about");
 
 function handleProfileFormSubmit(evt) {
   evt.preventDefault();
-  inputName.value = profileName.textContent;
-  inputAbout.value = profileAbout.textContent;
+  profileName.textContent = inputName.value;
+  profileAbout.textContent = inputAbout.value;
   popup.classList.remove("popup_opened");
 }
 
